abd1bd7173ec48613cc335a72519395f7bc923b5
Grim <gf.field@cox.net>
2019-02-09 14:30:50 -0600

    worked on arm code

--- TheActualRealOof/src/main/java/org/usfirst/frc3991/TheActualRealOof/Constants.java
+++ TheActualRealOof/src/main/java/org/usfirst/frc3991/TheActualRealOof/Constants.java
@@ -5,10 +5,25 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package org.usfirst.frc3991.TheActualRealOof;
+package org.usfirst.frc3991.TheActualRealOof
 
 /**
  * Add your docs here.
+ * 
+ * 
  */
 public class Constants {
+    //set PID slot for all
+    public static final int kSlotIdx = 0;
+    //set PID loop for all
+    public static final int kPIDLoopIdx = 0;
+    //set timeout setting
+    public static final int kTimeoutMs = 0;
+    //set constansts for different outputs
+    //Gains(kp, ki, kd, kf, izone, peak output);
+    static final Gains kGains = new Gains(0.15, 0.0, 1.0, 0.0, 0, 1.0);
+    }
+    
+
+
 }
